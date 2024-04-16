a97aa3fb4f888ce8f3db9dab0b7f82c9e5598a4f
JZ <juzhangds@gmail.com>
2024-04-16 07:23:02 -0700

    adding TDNN

--- egs/tedlium3/ASR/conformer_ctc2/conformer.py
+++ egs/tedlium3/ASR/conformer_ctc2/conformer.py
@@ -1031,3 +1031,73 @@ class ConvolutionModule(nn.Module):
         x = self.pointwise_conv2(x)  # (batch, channel, time)
 
         return x.permute(2, 0, 1)
+
+
+## adding the TDNN model
+class Tdnn(nn.Module):
+    def __init__(self, num_features: int, output_dim: int):
+        """
+        Args:
+          num_features:
+            Model input dimension.
+          ouput_dim:
+            Model output dimension
+        """
+        super().__init__()
+
+        # Note: We don't use paddings inside conv layers
+        self.tdnn = nn.Sequential(
+            nn.Conv1d(
+                in_channels=num_features,
+                out_channels=32,
+                kernel_size=3,
+            ),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm1d(num_features=32, affine=False),
+            nn.Conv1d(
+                in_channels=32,
+                out_channels=32,
+                kernel_size=5,
+                dilation=2,
+            ),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm1d(num_features=32, affine=False),
+            nn.Conv1d(
+                in_channels=32,
+                out_channels=32,
+                kernel_size=5,
+                dilation=4,
+            ),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm1d(num_features=32, affine=False),
+        )
+        self.output_linear = nn.Linear(in_features=32, out_features=output_dim)
+
+    def forward(self, x: torch.Tensor, x_lens: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+          x:
+            The input tensor with shape (T, N, C)
+          x_lens:
+            It contains the number of frames in each utterance in x
+            before padding.
+
+        Returns:
+          Return a tuple with 2 tensors:
+
+            - logits, a tensor of shape (T, N, C)
+            - logit_lens, a tensor of shape (N,)
+        """
+        # x = x.permute(0, 2, 1)  # (N, T, C) -> (N, C, T)
+
+        x = x.permute(1, 2, 0)  # (T, N, C) -> (N, C, T)
+        x = self.tdnn(x)
+        x = x.permute(2, 0, 1)  # (N, C, T) -> (T, N, C)
+        logits = self.output_linear(x)
+
+        # the first conv layer reduces T by 3-1 frames
+        # the second layer reduces T by (5-1)*2 frames
+        # the second layer reduces T by (5-1)*4 frames
+        # Number of output frames is 2 + 4*2 + 4*4 = 2 + 8 + 16 = 26
+        x_lens = x_lens - 26
+        return logits, x_lens
