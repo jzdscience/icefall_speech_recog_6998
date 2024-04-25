f5d18ac7945469a5619f44ab5efcf20f6d62508b
jzdscience <juzhangds@gmail.com>
2024-04-25 05:47:58 +0000

    adding decoding script for mixture model

--- egs/tedlium3/ASR/run_decoding.sh
+++ egs/tedlium3/ASR/run_decoding.sh
@@ -33,5 +33,21 @@ world_size=`nvidia-smi  -L | wc -l`
 ### TDNN7 (512 3k 0d -  512 3k 0d - 512 3k 0d - Statspooling - Double Swish )
  ./conformer_ctc2/decode.py --epoch 30  --exp-dir ./conformer_ctc2/exps/exp_all_tdnn_tdnn7 --result-dir ./conformer_ctc2/exps/exp_all_tdnn_tdnn7  --max-duration 400 --method ctc-decoding --use-averaged-model True --avg 5 --num-encoder-layers 4 --num-decoder-layers 2 --conv-type all_tdnn --tdnn-type tdnn7
 
+### TDNN8 (176 3k 0d -  176 3k 0d - 176  3k 0d - Double Swish)
+#  ./conformer_ctc2/decode.py --epoch 30  --exp-dir ./conformer_ctc2/exps/exp_all_tdnn_tdnn8 --result-dir ./conformer_ctc2/exps/exp_all_tdnn_tdnn8  --max-duration 400 --method ctc-decoding --use-averaged-model True --avg 5 --num-encoder-layers 4 --num-decoder-layers 2 --conv-type all_tdnn --tdnn-type tdnn8
+
 ### TDNN2NoSKIP (512 3k 0d -  512 3k 0d - 512 3k 0d )
 #  ./conformer_ctc2/decode.py --epoch 30  --exp-dir ./conformer_ctc2/exps/exp_all_tdnn_tdnn2_noskip --result-dir ./conformer_ctc2/exps/exp_all_tdnn_tdnn2_noskip  --max-duration 400 --method ctc-decoding --use-averaged-model True --avg 5 --num-encoder-layers 4 --num-decoder-layers 2 --conv-type all_tdnn_no_skip --tdnn-type tdnn2
+
+### Conv_TDNN8 (c-c-t-t)
+#  ./conformer_ctc2/decode.py --epoch 30  --exp-dir ./conformer_ctc2/exps/exp_tdnn8_conv_c_c_t_t --result-dir ./conformer_ctc2/exps/exp_tdnn8_conv_c_c_t_t  --max-duration 400 --method ctc-decoding --use-averaged-model True --avg 5 --num-encoder-layers 4 --num-decoder-layers 2 --conv-type c_c_t_t --tdnn-type tdnn8
+
+### Conv_TDNN8 (t-t-c-c)
+#  ./conformer_ctc2/decode.py --epoch 30  --exp-dir ./conformer_ctc2/exps/exp_tdnn8_conv_t_t_c_c --result-dir ./conformer_ctc2/exps/exp_tdnn8_conv_t_t_c_c  --max-duration 400 --method ctc-decoding --use-averaged-model True --avg 5 --num-encoder-layers 4 --num-decoder-layers 2 --conv-type t_t_c_c --tdnn-type tdnn8
+
+### Conv_TDNN8 (c-t-t-c)
+#  ./conformer_ctc2/decode.py --epoch 30  --exp-dir ./conformer_ctc2/exps/exp_tdnn8_conv_c_t_t_c --result-dir ./conformer_ctc2/exps/exp_tdnn8_conv_c_t_t_c  --max-duration 400 --method ctc-decoding --use-averaged-model True --avg 5 --num-encoder-layers 4 --num-decoder-layers 2 --conv-type c_t_t_c --tdnn-type tdnn8
+
+### Conv_TDNN8 (t-c-c-t)
+#  ./conformer_ctc2/decode.py --epoch 30  --exp-dir ./conformer_ctc2/exps/exp_tdnn8_conv_t_c_c_t --result-dir ./conformer_ctc2/exps/exp_tdnn8_conv_t_c_c_t  --max-duration 400 --method ctc-decoding --use-averaged-model True --avg 5 --num-encoder-layers 4 --num-decoder-layers 2 --conv-type t_c_c_t --tdnn-type tdnn8
+
